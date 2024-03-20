93c4c7b3795f7b9675807d57d7eabbf6dd4f4f39
ThiagoPVLima <ronnyy94@hotmail.com>
2024-03-20 01:11:23 -0300

    adicionei um js pra colocar uns sons

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
-var img_velho = document.getElementById('img-velho')
-var som_velho = document.getElementById('som-velho')
+const imagens = document.querySelectorAll('.minha-imagem')
+const imagem = document.getElementById('hero_cup')
+const miau1 = document.getElementById('miau1')
+const miau2 = document.getElementById('miau2')
 
-img_velho.addEventListener('click', function(){
-  if(som_velho.paused) {
-    som_velho.play();
+let contadorCliques = 0
+
+let audioAtual
+
+imagens.forEach(function (imagem, index) {
+  const audio = document.getElementById('audio' + (index + 1))
+
+  imagem.addEventListener('click', function () {
+    audio.play()
+  })
+})
+
+imagem.addEventListener('click', function () {
+  contadorCliques++;
+  if (contadorCliques <= 3) {
+    miau1.play()
   } else {
-    som_velho.pause();
-    som_velho.currentTime = 0
-    som_velho.play();
+    miau2.play()
+    contadorCliques = 0
   }
-});
+})
